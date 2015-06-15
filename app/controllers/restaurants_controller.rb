26596a4c073133da2b3c2690fb2d07ea7bd28454
Rocco <roccorighi@hotmail.com>
2015-06-15 16:24:53 +0100

    adds 3rd test and 'add restaurant' feature

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,17 @@
 class RestaurantsController < ApplicationController
   def index
-    # raise 'Hello from the index action'
+    @restaurants = Restaurant.all
+  end
+
+  def new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end

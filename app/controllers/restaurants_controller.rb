9d262e910ecf1f9e322ce99efeedd34089350b28
Rocco <roccorighi@hotmail.com>
2015-06-15 17:37:34 +0100

    adds edit restaurant feature with passing test

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,4 +19,15 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
+  end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -43,4 +43,17 @@ feature 'restaurants' do
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
+
+  context 'editing restaurants' do
+    before{Restaurant.create name: 'KFC'}
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link('Edit KFC')
+      fill_in('Name', with: 'Kentucky Fried Chicken')
+      click_button 'Update Restaurant'
+      expect(page).to have_content('Kentucky Fried Chicken')
+      expect(current_path).to eq '/restaurants'
+    end
+  end
 end

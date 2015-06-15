bcebd67c75431d673ff82976272abf929fdd734d
Rocco <roccorighi@hotmail.com>
2015-06-15 18:27:17 +0100

    adds adding review. test not passing

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant.reviews.create(review_params)
+  end
+
+  def review_params
+    params.require(:review).permit(:thoughts, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   get 'restaurants' => 'restaurants#index'
-  resources :restaurants
+  resources :restaurants do
+
+    resources :reviews
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

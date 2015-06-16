c921a9dda46965eb3c1bdf28df662c40bef7a394
Anna <anna.klimas@gmail.com>
2015-06-16 10:06:27 +0100

    Adds devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'restaurants' => 'restaurants#index'
+  root to: 'restaurants#index'
   resources :restaurants do
 
-    resources :reviews
+  resources :reviews
   end
 
 

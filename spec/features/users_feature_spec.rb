0b83e11aeaa6b4a1b2420472a89c53c128c91e0b
andygout <andygout@hotmail.co.uk>
2015-06-16 11:15:40 +0100

    Added: User sign up/in/out functionality and tested

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'User can sign in and out' do
+  context 'User not signed in and on the home page' do
+    it 'should see a sign-in link and sign-up link' do
+      visit('/')
+      expect(page).to have_link('Sign in')
+      expect(page).to have_link('Sign up')
+    end
+
+    it 'should not see sign-out link' do
+      visit('/')
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+
+  context 'User signed in on the home page' do
+    before do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testpassword')
+      fill_in('Password confirmation', with: 'testpassword')
+      click_button('Sign up')
+    end
+
+    it 'should see sign-out link' do
+      visit('/')
+      expect(page).to have_link('Sign out')
+    end
+
+    it 'should not see a sign-in link and sign-up link' do
+      visit('/')
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
+end

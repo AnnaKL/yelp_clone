01e4047781935913b8efa34890e892bedff50ba4
Tim <timrobertson0122@gmail.com>
2015-06-18 17:00:46 +0100

    added imagemagik and paperclip

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
-
+  has_attached_file :image, :styles => { :medium => "300X300>", :thumb => "100x100>" }, :default_url => '/images/:style/missing.png'
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true

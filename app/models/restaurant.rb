bb9495fbbac9a486b150fbac750cad82ee745530
Anna <anna.klimas@gmail.com>
2015-06-18 10:31:36 +0100

    Add: build_review method updated

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,9 +3,4 @@ class Restaurant < ActiveRecord::Base
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
-
-  # def build_review(current_user, review_params)
-  #      # review_params[:user_id] = current_user
-  #      byebug
-  # end
 end
